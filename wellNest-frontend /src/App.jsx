6aabc0d13529616eedeafad5abb541f9469b92aa
SamidhaManjrekar <smanjrekar135@gmail.com>
2025-06-02 18:59:33 +0400

    path update

--- wellNest-frontend /src/App.jsx
+++ wellNest-frontend /src/App.jsx
@@ -2,11 +2,14 @@ import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import Authentication from './pages/Authentication/Authentication';
-import HomePage from './pages/HomePage/HomePage';
-import Message from './pages/Message/Message';
+import MainLayout from './pages/MainLayout/MainLayout';
 import LandingPage from './pages/LandingPage/LandingPage';
-import Communities from './pages/Communities/Communities'; 
+import Message from './pages/Message/Message';
+import Communities from './pages/Communities/Communities';
+import HomePage from './components/MiddlePart/MiddlePart';
+import ProfilePage from './pages/Profile/ProfilePage';
 import { getProfileAction } from './redux/Auth/auth.action';
+import CreateCommunityPage from './components/Communities/CreateCommunityForm';
 
 function App() {
   const auth = useSelector(state => state.auth);
@@ -22,11 +25,19 @@ function App() {
   return (
     <Router>
       <Routes>
+        {/* Public routes */}
         <Route path="/" element={<LandingPage />} />
         <Route path="/auth/*" element={<Authentication />} />
-        <Route path="/home/*" element={<HomePage /> } />
-        <Route path="/message" element={<Message /> } />
-        <Route path="/communities" element={<Communities />} /> 
+
+        {/* Protected layout (with sidebar) */}
+        <Route element={<MainLayout />}>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:id" element={<ProfilePage />} />
+          <Route path="/communities" element={<Communities />} />
+        </Route>
+        <Route path="/create-community" element={<CreateCommunityPage />} />
+        <Route path="/message" element={<Message />} />
+        {/* Fallback route */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
